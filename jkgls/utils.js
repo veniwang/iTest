594701a48abbd9975438de5cb964c36d11416650
veniwang <veniwang@126.com>
2020-08-10 06:40:59 +0800

    up

--- jkgls/utils.js
+++ jkgls/utils.js
@@ -1,5 +1,5 @@
 var num2word = ['A','B','C','D','E','F','G','H','I'];
-var err_msg = '回答错误,请再次选择';
+var err_msg_fmt = '回答错误.答案提示："{0}"';
 
 var h5_fmt_choice4radio = '<input type="radio" name="choice" id="c{2}" value="{0}" />{1}<br/>';
 var h5_fmt_choice4check = '<input type="checkbox" name="choice" id="c{2}" value="{0}" />{1}<br/>';
